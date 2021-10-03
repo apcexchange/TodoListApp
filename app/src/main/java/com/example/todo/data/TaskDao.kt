d54220c03ed329feb2e472ee8bd9bf85debfcb82
apcexchange <princepee10@gmail.com>
2021-10-03 16:15:51 +0100

    database modified

--- app/src/main/java/com/example/todo/data/TaskDao.kt
+++ app/src/main/java/com/example/todo/data/TaskDao.kt
@@ -1,13 +1,25 @@
 package com.example.todo.data
 
 import androidx.room.*
+import com.example.todo.ui.task.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTask(searchQuery : String) : Flow<List<Task>>
+
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted:Boolean) : Flow<List<Task>> =
+        when(sortOrder){
+            SortOrder.BY_DATE -> getTaskSortedByDateCreated(query,hideCompleted)
+            SortOrder.BY_NAME -> getTaskSortedByName(query,hideCompleted)
+        }
+
+
+    @Query("SELECT * FROM task_table WHERE(completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTaskSortedByName(searchQuery : String, hideCompleted : Boolean) : Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE(completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTaskSortedByDateCreated(searchQuery : String, hideCompleted : Boolean) : Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
